828e2778743690767f515ca2d143916c27727c72
JGenereux <jacegenereux@gmail.com>
2024-09-12 13:38:57 -0600

    Added routes for creating, removing, and deletion of reviews.

--- server/routes/reviews.js
+++ server/routes/reviews.js
@@ -2,9 +2,40 @@ const router = require("express").Router();
 const Reviews = require("../Models/reviews.model");
 
 //add review to database
-
-//get reviews from database
-
+router.route("/create").post(async (req, res) => {
+  try {
+    //creates a new review with the items provided in the post request
+    //Should contain a itemName, username, rating, & comment.
+    const newReview = await Reviews(req.body);
+    await newReview.save();
+  } catch (error) {
+    res.status(400).json("Error: " + error);
+  }
+});
+//get reviews for a specific item from database
+router.route("/:itemname").get(async (req, res) => {
+  try {
+    //finds all items in the database that belong to that specific item
+    const reviews = Reviews.find({ itemName: req.params.itemname });
+    res.json(reviews);
+  } catch (error) {
+    res.status(400).json("Error: " + error);
+  }
+});
 //update review in database
 
 //remove review from database
+router.route("/remove/:username").delete(async (req, res) => {
+  try {
+    const review = await Reviews.findOneAndDelete({
+      userName: req.params.username,
+    });
+
+    if (!review) {
+      return res.status(400).json("Error: " + error);
+    }
+    res.json(review);
+  } catch (error) {
+    res.status(400).json("Error: " + error);
+  }
+});
