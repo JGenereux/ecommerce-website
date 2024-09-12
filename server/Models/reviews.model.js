446ee93a7f09eba909832f2b7062dcf6fd82ca5e
JGenereux <jacegenereux@gmail.com>
2024-09-12 13:32:12 -0600

    Added itemname as a column in the reviews schema.

--- server/Models/reviews.model.js
+++ server/Models/reviews.model.js
@@ -1,12 +1,13 @@
 //Schema for review database
 //Each review is a row
-//The columns are name, rating, comment
+//The columns are itemname, name, rating, comment
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const ReviewSchema = new Schema(
   {
+    itemName: { type: String, required: true },
     userName: { type: String, required: true },
     rating: { type: Number, required: true },
     comment: { type: String, required: true },
