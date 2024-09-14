e71fcd0eb651cb1093be7d982f2bb56b093ea962
JGenereux <jacegenereux@gmail.com>
2024-09-13 23:14:57 -0600

    Added nodemailer package and built route for sending emails

---
+++ server/routes/emails.js
@@ -0,0 +1,33 @@
+const router = require("express").Router();
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: `${process.env.EMAIL}`,
+    pass: `${process.env.SECRET}`,
+  },
+});
+
+//just need a post request from the frontend
+router.route("/").post(async (req, res) => {
+  try {
+    const newEmail = await req.body;
+    const mailOptions = {
+      from: `${process.env.EMAIL}`,
+      to: `${newEmail.email}`,
+      subject: `${newEmail.subject}`,
+      text: `${newEmail.text}`,
+    };
+    transporter.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("Email sent: " + info.response);
+      }
+      res.json("Email sent successfully");
+    });
+  } catch (error) {
+    res.status(400).json("Error: " + error);
+  }
+});
